17c8d64bca5dfa696d1993b64783163f88d0ce6e
Jevy <jevyababa@gmail.com>
2023-02-07 16:48:21 +0800

    commit changes

---
+++ ngapp/dashboard/controller.js
@@ -0,0 +1,8 @@
+
+angular.module('app').controller('AdminDashboardCtrl', AdminDashboardCtrl)
+
+AdminDashboardCtrl.$inject = ['$scope', '$ocLazyLoad', '$injector', '$q', '$filter'];
+
+function AdminDashboardCtrl($scope, $ocLazyLoad, $injector, $q, filter) {
+    var vm = this;
+}
